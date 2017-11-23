de7fe86bb3746cb1bb07f6605408617ba4673ce8
MauRi <MauRi@10.5.1.6>
2017-11-23 17:34:50 -0500

    Mi aporte es el Hola Mauricio V2.0 en el Proyecto: PjtRepositorio001.java

--- src/pjtrepositorio001/PjtRepositorio001.java
+++ src/pjtrepositorio001/PjtRepositorio001.java
@@ -17,6 +17,8 @@ public class PjtRepositorio001 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mauricio!!");
+        
+        System.out.println("Hola Mauricio V 2.0");
     }
     
 }
